d0cf6de549d897f5d2294077231e8952f2b2efbd
Fouyoud <kenbibi39@gmail.com>
2025-02-04 13:04:50 +0100

    basic clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "./shared/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
   <div className="p-6">
-    <div className="w-100 flex justify-end">
+    <div className="w-100 flex gap-x-5 justify-end">
+      <UserButton/>
       <ThemeToggle />
     </div>
     <h1 className="font-bold text-blue-500 font-barlow">Welcome to the course !</h1>
